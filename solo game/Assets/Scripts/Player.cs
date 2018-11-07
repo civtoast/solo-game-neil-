7c042bfd4d7ae4006c0f47eee37cfd9301939a68
Neil van der merwe <necktro101@gmail.com>
2018-11-07 12:15:05 +0200

    made boosters

--- solo game/Assets/Scripts/Player.cs
+++ solo game/Assets/Scripts/Player.cs
@@ -7,16 +7,54 @@ public class Player : MonoBehaviour {
     public float thrust;
     public GameObject bulet;
     public GameObject spawnpoint;
+    [Space]
+    [Header("partical system")]
+    public GameObject down1;
+    public GameObject down2;
+    public GameObject right;
+    public GameObject left;
     //public Animator anim;
     void Start () {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();       
+        down1.SetActive(false);
+        down2.SetActive(false);
+        right.SetActive(false);
+        left.SetActive(false);
+
     }
-	
-	
-	void Update () {
+
+
+    void Update () {
         if (Input.GetButton("Vertical"))
         {
             rb.AddForce(transform.up * thrust);
+            
+            down1.SetActive(true);
+            down2.SetActive(true);
+
+        }
+        if (Input.GetButtonUp("Vertical"))
+        {
+
+            down1.SetActive(false);
+            down2.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            right.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            left.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            right.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            left.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
